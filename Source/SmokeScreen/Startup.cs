930921d37e6c6d8823cb492feaa955e9573cfed0
Lisias <me@lisias.net>
2020-12-13 13:06:29 -0300

    Adding Startup and KSPe logging facilities

---
+++ Source/SmokeScreen/Startup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SmokeScreen
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+	internal class Startup : MonoBehaviour
+	{
+		private void Start()
+		{
+			Log.force("Version {0}", Version.Text);
+
+			try
+			{
+				KSPe.Util.Installation.Check<Startup>(typeof(Version));
+			}
+			catch (KSPe.Util.InstallmentException e)
+			{
+				Log.error(e, this);
+				KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+			}
+		}
+	}
+}
