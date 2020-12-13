eaa492c48d0fbde7d05be5e3f057686b21508b15
Lisias <me@lisias.net>
2020-12-13 15:18:26 -0300

    Adding support for Module Manager

---
+++ Source/SmokeScreen/ModuleManagerSupport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SmokeScreen
+{
+	public static class ModuleManagerSupport
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			string[] r = {typeof(ModuleManagerSupport).Namespace};
+			return r;
+		}
+	}
+}
